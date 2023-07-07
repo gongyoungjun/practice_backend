d9900772f932c9bf0b8f2cca7f823eba1a2ef53e
gongjam58 <gongjam58@gmail.com>
2023-07-07 17:06:37 +0900

    23.07.7.message properties

--- src/main/java/com/example/test/emp/controller/testController.java
+++ src/main/java/com/example/test/emp/controller/testController.java
@@ -2,16 +2,23 @@ package com.example.test.emp.controller;
 
 import com.example.test.emp.dto.testDTO;
 import com.example.test.emp.dao.test;
+import lombok.RequiredArgsConstructor;
+import org.springframework.context.MessageSource;
 import org.springframework.web.bind.annotation.*;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Locale;
 
 @RestController
+@RequiredArgsConstructor
 public class testController {
 
     private List<test> users = new ArrayList<>();
     private int id = 0;
+
+
+    private final MessageSource messageSource;
 
     // User 추가
     @PostMapping("/user")
@@ -33,6 +40,21 @@ public class testController {
 
         return users;
     }
+    @GetMapping("/testcall")
+    public ArrayList<String> test (Locale locale){
+        ArrayList<String> msgs = new ArrayList<>();
 
+        msgs.add(messageSource.getMessage("test",null,locale ));
+
+        msgs.add(messageSource.getMessage("test",null, locale.ENGLISH));
+
+        msgs.add(messageSource.getMessage("test",null, locale.KOREA));
+
+
+        return msgs;
+    }
 
 }
+
+
+
